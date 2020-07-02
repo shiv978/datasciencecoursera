0915c25c1e1ecf68b8f37eb8c650004f51de8ca1
shiv978 <shiva.prasad@sjmsom.in>
2020-07-03 00:25:34 +0530

    End of Data types- matrices, factors etc

--- Course 2 - R Programming.R
+++ Course 2 - R Programming.R
@@ -47,3 +47,84 @@ l <- list("a", 6, TRUE, 9+8i, "wow this is cool")
 l   ## doesnt print like vector because all ojects are differnet, elements are indexed by double brackets, and list also have double brackets around them, hece you can know that they are lists
 
 
+
+##Matrices are also vectors in R, but special types of vectors with dimensions of row and columns 
+
+# Matrix are filled column wise, ie left most column gets filled first from top to bottm and the it moves right till end of columns are reached 
+
+m <- matrix(1:3, nrow =2 , ncol = 3)
+m
+
+n<- 1:10  ## take  vector
+dim(n)<-c(5,2)  # Provide the vector the dimnesions and create a matrix
+n
+
+
+## matrix can also created by cbinding and row binding 
+
+o <- 1:5
+p <- 10:14
+
+cbind(o,p)
+
+rbind(o,p)
+
+
+## Factors are used to represent categorical data - Çategory suçh as skin çolour - white blaçk brown çream etç 
+
+
+# used a lot in regressions lm() etc, 
+
+#it can also be ranked say assistant prof < associate prof < full prof
+
+q <- factor(c("brown skin", "creamy skin", "white skin", "black skin", "brown skin", "brown skin"))
+q
+table(q)
+unclass(q)
+
+## You can order using a levels function and you want yes > no, saying that yes is the 1st level
+
+q <- factor(c("yes","no","no", "yes", "yes","yes"), levels = c("yes","no"))
+q
+unclass(q)
+
+
+
+## Missing values are special types of objects  NaN- mathematical operations resulting NaN
+
+# we can use is.na() to decide whether our data set contains any missing values 
+#is.nan() is used for detecting undefined mathematical ops
+
+# Na can be character NA or interger NA, all NaN are NAs but vice versa is untrue
+
+
+r<- c(1,2,3, NA, 5,6,7)
+is.na(r)
+is.nan(r)
+
+## Data frames, used to store key data type in R and used to store tabular data
+
+# Represented as speical type of list where every element of the list has same length
+
+# in dataframes, every column is a list ie holds different data (think of your excel) and length of each list is the umber of rows 
+
+# Each column need not be of same type
+
+#crated by read.data() read.csv()
+
+# can be converted to matrix using data.matrix() matrix will coerce differnet data types though
+
+# foo is a integer vector 1:4, bar is a logical vector 
+
+s <- data.frame(foo= 1:4, bar = c(T,T,F,T))
+s
+
+
+# Names are used to create a very readable code-- Very Important
+
+  t <- 1:3 # this is a integer vector and you can name it
+  
+  names(t) <- c("mannu", "shiv", "aadya")
+  names(t)   
+  t
+  
